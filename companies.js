12407d32048c1d03e6a8d97e4283dc04a6c4b550
Abulfaz Sadigov <abulfaz.ps@code.local>
2022-10-07 12:38:35 +0400

    Card and Companies server added

--- companies.js
+++ companies.js
@@ -11,24 +11,22 @@ app.use(bodyParser.json());
 app.use(cors());
 
 mongoose.connect(
-  "mongodb+srv://gurban:300793mm@cluster0.qob0oxl.mongodb.net/company?retryWrites=true&w=majority",
+  "mongodb+srv://abulfaz:ebulfez1995@cluster0.hnqdmkq.mongodb.net/Companies?retryWrites=true&w=majority",
   { useNewUrlParser: true }
 );
 
 //DB TABLE
 const companySchema = new Schema({
   companyName: String,
-  email: String,
-  fullName: String,
-  password: String,
-  confirmPassword: String,
+  description: String,
+  price: Number,
 });
 
-const Company = mongoose.model("Company", companySchema);
+const Companies = mongoose.model("Company", companySchema);
 
 //GETALL
 app.get("/company", (req, res) => {
-  Company.find({}, (err, docs) => {
+  Companies.find({}, (err, docs) => {
     if (!err) {
       res.json(docs);
     } else {
@@ -41,7 +39,7 @@ app.get("/company", (req, res) => {
 app.get("/company/:id", (req, res) => {
   let id = req.params.id;
 
-  Company.findById(id, (err, doc) => {
+  Companies.findById(id, (err, doc) => {
     if (!err) {
       if (doc) res.json(doc);
       else res.status(404).json({ message: "Not found!" });
@@ -50,36 +48,30 @@ app.get("/company/:id", (req, res) => {
     }
   });
 });
+
 // Delete
 app.delete("/company/:id", (req, res) => {
   let id = req.params.id;
-  Company.findByIdAndDelete(id, (err) => {
+  Companies.findByIdAndDelete(id, (err) => {
     if (!err) res.json({ messagae: "Success!" });
     else res.status(500).json(err);
   });
 });
 
-//Post
 app.post("/company", (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  var company = new Company({
+  var company = new Companies({
     companyName: req.body.companyName,
-    email: req.body.email,
-    fullName: req.body.fullName,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    status: true,
+    description: req.body.description,
+    price: req.body.price,
   });
   company.save();
   res.send("Success!!");
 });
 
-//Login Post
-app.post("/company/login", (req, res) => {});
-
-app.listen(8080, () => {
+app.listen(8000, () => {
   console.log("Server is running!!");
 });
